24bcefdad15b9ffdadaa6eaa2bf87c23e57acedb
Kiran <kiran.ahirefire2000@gmail.com>
2021-12-03 22:36:59 +0530

    BLOGS

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyBniJZhWSBBHlhwjFbQrvTHDKEry0rrYzE",
+    authDomain: "portfolio-2c02b.firebaseapp.com",
+    projectId: "portfolio-2c02b",
+    storageBucket: "portfolio-2c02b.appspot.com",
+    messagingSenderId: "939483568480",
+    appId: "1:939483568480:web:fc8548632e48332d782605",
+    measurementId: "G-VMWV3XGR50"
+  }
 };
